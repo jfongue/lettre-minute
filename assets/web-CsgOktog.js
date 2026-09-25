f69b4fea978e144d77a79c9f01a06602bccdcfad
jfongue <fongue.jeremy@gmail.com>
2026-09-25 08:40:47 +0200

    Publish the web version (8944edd)

---
+++ assets/web-CsgOktog.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-WUCApMLK.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
--- assets/web-DIUqC0qo.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./index-BBjHh6UW.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
