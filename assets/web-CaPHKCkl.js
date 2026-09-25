68a2245288aad368d8059712027f9726aec331ec
jfongue <fongue.jeremy@gmail.com>
2026-09-25 08:48:05 +0200

    Publish the web version (0fa9125)

--- assets/web-CaPHKCkl.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./index-BDeRGSm7.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
---
+++ assets/web-DBItJods.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-D6M2kcsg.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
