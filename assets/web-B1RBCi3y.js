e1fcbe3cb3a56d44a73fca44cc2ced5c2776b2ac
jfongue <fongue.jeremy@gmail.com>
2026-09-25 09:39:12 +0200

    Publish the web version (48b5023)

---
+++ assets/web-B1RBCi3y.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-Bgc_1jy3.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
--- assets/web-RayLn8FS.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./index-CI0nPoCJ.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
